59ca018811ca965af28da1e818d2dfe601bb7a24
yatender9868 <kumaryatender445@gmail.com>
2025-10-26 11:13:36 +0530

    test commit

--- pandas/intro_pandas.py
+++ pandas/intro_pandas.py
@@ -1,21 +1,23 @@
 import pandas as pd
 
 
-a=pd.Series([1,2,3])
+# a=pd.Series([1,2,3])
 
-print(a,type(a))
+# print(a,type(a))
 
 
 # series with index
 b=pd.Series(["yatender", "golu", 'mayank'],index=[1,2,3])
 
-print(b,type(b))
+print(b)
+print(b[0:2])
 
 dictionary=pd.Series({1:"golu",
                       2:"mayank",
                       3:"anand"})
 
-print(dictionary,type(dictionary))
+print(dictionary)
+print(dictionary[1:3])
 
 
 
